5ce2fb873a5dfe38d608bbb4e793a13bb80b41bc
jjun0214z <jjun0214z@gmail.com>
2019-06-10 17:25:50 +0900

    useTitle 추가

---
+++ src/common/hooks/useTitle.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+
+export default initialTitle => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
--- src/components/HooksTest.js
+++ src/components/HooksTest.js
@@ -1,23 +1,12 @@
 import React from "react";
-import useInput from "common/hooks/useInput";
-import useTabs from "common/hooks/useTabs";
-
-const content = [
-  { tab: "section 1", content: "section 1 content" },
-  { tab: "section 2", content: "section 2 content" }
-];
+import useTitle from "common/hooks/useTitle";
 
 const TestComponent = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
+  const titleUpdate = useTitle("loading");
+  setTimeout(() => titleUpdate("Welcome"), 5000);
   return (
     <div>
       <div>Hooks TEST</div>
-      {content.map((section, idx) => (
-        <button onClick={() => changeItem(idx)} key={idx}>
-          {section.tab}
-        </button>
-      ))}
-      <div>{currentItem.content}</div>
     </div>
   );
 };
