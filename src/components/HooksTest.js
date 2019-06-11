3ad90ea5a52ad2b4532037594d22ac726410ce55
jjun0214z <jjun0214z@gmail.com>
2019-06-11 11:04:37 +0900

    ref useClick 추가

--- src/components/HooksTest.js
+++ src/components/HooksTest.js
@@ -1,9 +1,12 @@
 import React from "react";
+import useClick from "common/hooks/useClick";
 
 const TestComponent = () => {
+  const sayHello = () => console.log("hello");
+  const title = useClick(sayHello);
   return (
     <div>
-      <div>Hooks TEST</div>
+      <div ref={title}>Hooks TEST</div>
     </div>
   );
 };
